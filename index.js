6a9820ea323d9bdec4527f815d406ac9956b2e5b
Aleph Lennin <alephlennin.dev@outlook.com>
2024-11-12 03:27:43 -0300

    Update index.js
    formatação do codigo

--- index.js
+++ index.js
@@ -1,27 +1,31 @@
 class hero {
-    constructor(name, age, ninja, atacou, boss){
-        this.name = name
-        this.age = age
-        this.type = ninja
-        this.atacou = atacou
-        this.boss = boss
+  constructor(name, age, ninja, atacou, boss) {
+    this.name = name;
+    this.age = age;
+    this.type = ninja;
+    this.atacou = atacou;
+    this.boss = boss;
+  }
+  atacar() {
+    for (let hp = 6; hp > 0; hp--) {
+      console.log(`O ${this.type} atacou usando ${this.atacou}`);
+      console.log(`Dano aplicado... HP restante: ${hp}`);
     }
-atacar(){
-    for  (let hp = 6; hp > 0; hp--){
-        console.log(`O ${this.type} atacou usando ${this.atacou}`)
-        console.log(`Dano aplicado... HP restante: ${hp}`)
-    }
-}
-derrotou(){
-    if (win === true){
-        console.log(`O jogador ${this.name} de ${this.age} anos, derrotou o ${this.boss}`)
-    }else if (win === false){
-        console.log(`O jogador ${this.name} de ${this.age} anos, foi derrotado pelo ${this.boss}`)
+  }
+  derrotou() {
+    if (win === true) {
+      console.log(
+        `O jogador ${this.name} de ${this.age} anos, derrotou o ${this.boss}`
+      );
+    } else if (win === false) {
+      console.log(
+        `O jogador ${this.name} de ${this.age} anos, foi derrotado pelo ${this.boss}`
+      );
     }
   }
 }
 
-let win = true
-let player = new hero("Aleph", 25, "ninja", "shuriken", "Ogro do Pantano")
-player.atacar()
-player.derrotou()
+let win = true;
+let player = new hero("Aleph", 25, "ninja", "shuriken", "Ogro do Pantano");
+player.atacar();
+player.derrotou();
